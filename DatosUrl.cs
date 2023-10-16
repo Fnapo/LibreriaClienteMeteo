26bb9eb121018dd924773b3cf5e9b4f2402d7f23
Fnapo <36490842+Fnapo@users.noreply.github.com>
2023-10-16 14:19:20 +0200

    Agregar archivos de proyecto.

---
+++ DatosUrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LibClienteMeteo
+{
+	///
+	/// <summary>
+	/// Almacen de la URL fuente de los datos.
+	/// </summary>
+	/// <param name="Estado">
+	/// Nos indice el estado de la respuesta: 200, 404.
+	/// </param>
+	/// <param name="URL">
+	/// La URL fuente de los datos útiles.
+	/// </param>
+	///
+	public record class DatosUrl(
+        [property: JsonPropertyName("estado")] int Estado,
+        [property: JsonPropertyName("datos")] string URL
+    );
+}
